e116bba6e483341b06bbb08d838228db44e4eae6
axy <axy@hedaokeji.com>
2018-03-21 15:01:02 +0800

    GraphView提交

--- app/src/main/java/com/example/view/PathView.java
+++ app/src/main/java/com/example/view/PathView.java
@@ -3,10 +3,14 @@ package com.example.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
+import android.graphics.Rect;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -25,12 +29,18 @@ public class PathView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 //        pathText(canvas);
-        paintBezier(canvas);
+//        paintBezier(canvas);
+        pathOp(canvas);
     }
 
+    /**
+     * 贝塞尔曲线
+     * @param canvas
+     */
     private void paintBezier(Canvas canvas){
         PointF startPoint,controlPoint,endPoint;
 
@@ -84,20 +94,77 @@ public class PathView extends View {
         canvas.drawPath(path,paint);
     }
 
+    /**
+     * Path的Op探索
+     * @param canvas
+     */
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void pathOp(Canvas canvas){
+        drawPathOp(canvas,new PointF(0,0), Path.Op.DIFFERENCE,"DIFFERENCE");
+        //注：第二次的坐标是相对于第一次移动之后的
+        drawPathOp(canvas,new PointF(500,0), Path.Op.INTERSECT,"INTERSECT");
+
+        drawPathOp(canvas,new PointF(-500,500), Path.Op.UNION,"UNION");
+
+        drawPathOp(canvas,new PointF(500,0), Path.Op.XOR,"XOR");
+
+        drawPathOp(canvas,new PointF(-500,500), Path.Op.REVERSE_DIFFERENCE,"REVERSE_DIFFERENCE");
+    }
+
+    /**
+     * 绘制每一个圆
+     * @param canvas
+     * @param start
+     * @param op
+     */
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void drawPathOp(Canvas canvas, PointF start, Path.Op op,String title){
+        //1.将坐标系原点移到start位置
+        canvas.translate(start.x,start.y);
+
+        //2.定义圆的圆心和半径
         PointF circle1  = new PointF(200,200);
-        PointF cicile2  = new PointF(350,350);
+        PointF cicile2  = new PointF(250,250);
         int radius = 100;
+
+        //3.定义Path1和Path2的区域
         Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.FILL);
         Path path1 = new Path();
         path1.addCircle(circle1.x,circle1.y,radius, Path.Direction.CW);
-
         Path path2 = new Path();
         path2.addCircle(cicile2.x,cicile2.y,radius, Path.Direction.CW);
 
+        //注：Path3和Path4是Path1和Path2拷贝
+        Path path3 = new Path();
+        path3.addCircle(circle1.x,circle1.y,radius, Path.Direction.CW);
+        Path path4 = new Path();
+        path4.addCircle(cicile2.x,cicile2.y,radius, Path.Direction.CW);
+
+
+        //4.画Op后的区域
+        path3.op(path4, op);
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawPath(path3,paint);
+
+
+        //5.画path1
+        paint.setPathEffect(new DashPathEffect(new float[]{10,5},10));//设置虚线
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.BLUE);
         canvas.drawPath(path1,paint);
+
+        //6.画path2
+        paint.setColor(Color.RED);
         canvas.drawPath(path2,paint);
+
+        //7.绘制文字
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(30);
+        Rect mBound = new Rect();
+        paint.getTextBounds(title, 0, title.length(), mBound);
+        canvas.drawText(title,225-mBound.width()/2,430,paint);
     }
 }
